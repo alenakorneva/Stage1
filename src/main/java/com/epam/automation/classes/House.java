4a7f4762706155c454b11d2b0f29a7fbb38dbf4c
Alena Korneva <ekorneva323@gmail.com>
2022-01-13 21:06:07 +0300

    add changes to Java Classes package MainTask according to second request from Test Automation Lab

--- src/main/java/com/epam/automation/classes/House.java
+++ src/main/java/com/epam/automation/classes/House.java
@@ -2,11 +2,11 @@ package com.epam.automation.classes;
 
 public class House {
     private int id;
-    private int flat;
+    private int flatNumber;
     private String street;
 
     public class ExtraHouseInformation {
-        private double square;
+        private double flatSquare;
         private int floor;
         private int roomsAmount;
         private String buildingType;
@@ -15,17 +15,17 @@ public class House {
 
     ExtraHouseInformation extraHouseInformation = new ExtraHouseInformation();
 
-    public House(int id, int flat, int roomsAmount, double square, int floor) {
+    public House(int id, int flatNumber, int roomsAmount, double square, int floor) {
         this.id = id;
-        this.flat = flat;
+        this.flatNumber = flatNumber;
         extraHouseInformation.roomsAmount = roomsAmount;
-        extraHouseInformation.square = square;
+        extraHouseInformation.flatSquare = square;
         extraHouseInformation.floor = floor;
     }
 
-    public House(int id, int flat, int floor, int roomsAmount) {
+    public House(int id, int flatNumber, int floor, int roomsAmount) {
         this.id = id;
-        this.flat = flat;
+        this.flatNumber = flatNumber;
         extraHouseInformation.floor = floor;
         extraHouseInformation.roomsAmount = roomsAmount;
     }
@@ -38,20 +38,20 @@ public class House {
         this.id = id;
     }
 
-    public int getFlat() {
-        return flat;
+    public int getFlatNumber() {
+        return flatNumber;
     }
 
-    public void setFlat(int flat) {
-        this.flat = flat;
+    public void setFlatNumber(int flatNumber) {
+        this.flatNumber = flatNumber;
     }
 
     public double getSquare() {
-        return extraHouseInformation.square;
+        return extraHouseInformation.flatSquare;
     }
 
     public void setSquare(double square) {
-        extraHouseInformation.square = square;
+        extraHouseInformation.flatSquare = square;
     }
 
     public int getFloor() {
@@ -98,8 +98,8 @@ public class House {
     public String toString() {
         return "House{" +
                 "id=" + id +
-                ", flat=" + flat +
-                ", square=" + extraHouseInformation.square +
+                ", flat=" + flatNumber +
+                ", square=" + extraHouseInformation.flatSquare +
                 ", floor=" + extraHouseInformation.floor +
                 ", roomsAmount=" + extraHouseInformation.roomsAmount +
                 ", street='" + street + '\'' +
--- src/main/java/com/epam/automation/classes/HouseList.java
+++ src/main/java/com/epam/automation/classes/HouseList.java
@@ -7,36 +7,42 @@ public class HouseList {
 
     {
         houses.add(new House(1, 111, 3, 35.6, 3));
-        houses.add(new House(1, 222, 2, 43.5, 4));
-        houses.add(new House(1, 333, 1, 55.8, 5));
-        houses.add(new House(1, 444, 2, 110.4, 6));
-        houses.add(new House(1, 555, 3, 65.3, 7));
-        houses.add(new House(1, 666, 2, 74.3, 5));
-        houses.add(new House(1, 777, 1, 40.7, 23));
-        houses.add(new House(1, 888, 2, 96.7, 1));
+        houses.add(new House(2, 222, 2, 43.5, 4));
+        houses.add(new House(3, 333, 1, 55.8, 5));
+        houses.add(new House(4, 444, 2, 110.4, 6));
+        houses.add(new House(5, 555, 3, 65.3, 7));
+        houses.add(new House(6, 666, 2, 74.3, 5));
+        houses.add(new House(7, 777, 1, 40.7, 23));
+        houses.add(new House(8, 888, 2, 96.7, 1));
     }
 
-    public void getFlatsByRoomsAmount(int roomsAmount) {
+    public int getFlatsByRoomsAmount(int roomsAmount) {
         System.out.println("Flats with rooms amount: " + roomsAmount);
+        int flatNumberWIthRequiredRoomsAmount = 0;
         for (House house : houses) {
             if (house.getRoomsAmount() == roomsAmount)
-                System.out.println(house.getFlat());
+                return flatNumberWIthRequiredRoomsAmount = house.getFlatNumber();
         }
+        return flatNumberWIthRequiredRoomsAmount;
     }
 
-    public void getFlatsByRoomsAmountAndFloor(int roomsAmount, int initialFloor, int endFloor) {
+    public int getFlatsByRoomsAmountAndFloor(int roomsAmount, int initialFloor, int endFloor) {
         System.out.println("Flats with rooms amount: " + roomsAmount + " on the floor between " + initialFloor + " and " + endFloor);
+        int flatNumberWIthRequiredRoomsAmountAndFloor = 0;
         for (House house : houses) {
             if (house.getRoomsAmount() == roomsAmount && house.getFloor() >= initialFloor && house.getFloor() <= endFloor)
-                System.out.println(house.getFlat());
+                return flatNumberWIthRequiredRoomsAmountAndFloor = house.getFlatNumber();
         }
+        return flatNumberWIthRequiredRoomsAmountAndFloor;
     }
 
-    public void getFlatBySquare(double square) {
+    public int getFlatBySquare(double square) {
         System.out.println("Flats with square more than " + square + " square meters");
+        int flatNumberWIthRequiredSquare = 0;
         for (House house : houses) {
             if (house.getSquare() > square)
-                System.out.println(house.getFlat());
+                return flatNumberWIthRequiredSquare = house.getFlatNumber();
         }
+        return flatNumberWIthRequiredSquare;
     }
 }
